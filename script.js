bb11690e3d08a778b60b92987d68592811744b48
Carlos.Inhani <carlos.inhani@gmail.com>
2020-10-08 10:00:26 -0300

    Putting_style_and_finishing_the_snake_game

--- script.js
+++ script.js
@@ -40,10 +40,18 @@ function update (event){
 
 
 function iniciarJogo() {
+
     if(snake[0].x > 15 * box && direction == "rigth") snake[0].x = 0;
-    if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].x < 0  && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0  && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++ ){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('GAME OVER :(');
+        }
+    }
  
     criarBG();
     criarCobrinha(); 
@@ -55,10 +63,16 @@ function iniciarJogo() {
     if (direction == "rigth") snakeX += box;
     if (direction == "left") snakeX -= box;
     if (direction == "up") snakeY -= box;
-    if (direction == "dowm") snakeY += box;
+    if (direction == "down") snakeY += box;
 
-    snake.pop();
-
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1 ) * box,
+        food.y = Math.floor(Math.random() * 15 + 1 ) * box
+    } 
+    
     let newHead = {
         x: snakeX,
         y: snakeY
